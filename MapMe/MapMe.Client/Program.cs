fdc3af40cdccb64a133f659e5a92fa52e4b5cc12
Adam Zaplatílek <adam.zaplatilek@gmail.com>
2025-08-13 18:34:57 +0700

    feat: register ChatService in DI container

--- MapMe/MapMe.Client/Program.cs
+++ MapMe/MapMe.Client/Program.cs
@@ -11,5 +11,6 @@ builder.Services.AddScoped(sp => new HttpClient
     BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
 });
 builder.Services.AddScoped<UserProfileService>();
+builder.Services.AddScoped<ChatService>();
 
 await builder.Build().RunAsync();
